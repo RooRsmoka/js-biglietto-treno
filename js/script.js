8821cdef3a711b6b574a1c4659ebeb2bd10a0dab
RooRsmoka <chri.disa96@gmail.com>
2021-12-03 22:40:29 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -7,4 +7,36 @@
     // OPPURE SE over 65 applichiamo uno sconto del 40%,
     // ALTRIMENTI il prezzo non ha sconto.
     // Stampiamo a video il prezzo del bigletto con l'utilizzo dei decimali.
-*/ 
+*/
+
+// Creo L'elemento in cui sarà mostrato l'output
+const outputHtml = document.getElementById('output');
+
+// Chiedo all'utente la distanza da percorrere.
+const userDistance = parseInt(prompt('Inserisci la distanza che devi percorrere(in Km).'));
+
+// Chiedo all'utente L'età.
+const userAge = parseInt(prompt('Inserisci la tua età.'));
+
+// Calcolo il prezzo del biglietto in base ai Km inseriti(km * 0.21 €).
+const travelPrice = userDistance * 0.21;
+// Calcolo lo sconto per i minorenni.
+let discountTwenty = (travelPrice - (travelPrice * 20) / 100);
+discountTwenty = discountTwenty.toFixed(2);
+// Calcolo lo sconto per gli over 65.
+let discountForty = (travelPrice - (travelPrice * 40) / 100);
+discountForty = discountForty.toFixed(2)
+
+
+// Ora basandoci sull'età inserita dall'utente
+// definiamo i casi in cui effettuiamo lo sconto.
+if (userAge < 18) {
+    outputHtml.innerHTML = `Il prezzo del biglietto è di ${discountTwenty} €.`
+} else if (userAge >= 65) {
+    outputHtml.innerHTML = `Il prezzo del biglietto è di ${discountForty} €.`
+} else {
+    outputHtml.innerHTML = `Il prezzo del biglietto è di ${travelPrice} €.`
+}
+
+console.log(userDistance);
+console.log(userAge);
