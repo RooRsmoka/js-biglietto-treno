09a66328efda08b7027fbfab87e231cb50030a45
RooRsmoka <chri.disa96@gmail.com>
2021-12-03 15:28:35 +0100

    creazione file e struttura

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*Programma che calcola il prezzo del biglietto del treno.
+    // Chiedo all'utente la distanza in Km che deve percorrere.
+    // Chiedo all'utente l'età.
+    // Il prezzo del biglietto è calcolato tramite la seguente operazione (0.21€ / Km).
+    // Inoltre, basandoci sull'età dell'utente verifichiamo se ha diritto ad una scontistica.
+    // SE minorenne applichiamo uno sconto del 20%,
+    // OPPURE SE over 65 applichiamo uno sconto del 40%,
+    // ALTRIMENTI il prezzo non ha sconto.
+    // Stampiamo a video il prezzo del bigletto con l'utilizzo dei decimali.
+*/ 
